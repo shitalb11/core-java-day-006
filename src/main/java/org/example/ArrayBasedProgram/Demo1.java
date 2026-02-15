948e0a2d3b9cc33c0aee984c3227c02560e3eaa5
Shital Bhosale <shitalb075@gmail.com>
2026-02-15 22:10:18 +0530

    demo1

---
+++ src/main/java/org/example/ArrayBasedProgram/Demo1.java
@@ -0,0 +1,9 @@
+package org.example.ArrayBasedProgram;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int[] a={1,2,3};
+        for (int i : a) //used for-each loop
+            System.out.println(i + " ");
+    }
+}
