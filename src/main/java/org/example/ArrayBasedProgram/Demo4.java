4d8c45b2be11ee79ecf9e2ca6dce907b81eaa361
Shital Bhosale <shitalb075@gmail.com>
2026-02-16 15:47:51 +0530

    DEMO4

--- src/main/java/org/example/ArrayBasedProgram/Demo4.java
+++ src/main/java/org/example/ArrayBasedProgram/Demo4.java
@@ -2,7 +2,7 @@ package org.example.ArrayBasedProgram;
 
 public class Demo4 {
     public static void main(String[] args) {
-        int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        int[] array = {10,20,30,40,50,60,70,80,90};
 
         for (int i=0;i<array.length;i++){
             System.out.println(array[i]+",");
