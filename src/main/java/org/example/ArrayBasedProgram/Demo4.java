5b608fe1b3181799ad1a92b847c95965addae9e6
Shital Bhosale <shitalb075@gmail.com>
2026-02-16 15:45:48 +0530

    demo4

---
+++ src/main/java/org/example/ArrayBasedProgram/Demo4.java
@@ -0,0 +1,13 @@
+package org.example.ArrayBasedProgram;
+
+public class Demo4 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+
+        for (int i=0;i<array.length;i++){
+            System.out.println(array[i]+",");
+        }
+    }
+}
+
+//printing array in vertical line
