d79bf88af5cc6cc8289cf9b5a87ef2bf291a19ad
Shital Bhosale <shitalb075@gmail.com>
2026-02-16 15:20:30 +0530

    demo2

---
+++ src/main/java/org/example/ArrayBasedProgram/Demo2.java
@@ -0,0 +1,15 @@
+package org.example.ArrayBasedProgram;
+
+public class Demo2 {
+  public static void main(String[] args) {
+    int[] array1 = {};
+    int[] array2 = new int['A'];
+    int[] array3 = {10, 30, 40, 50, 60, 70, 80, 90, 100};
+
+    System.out.println("size of array1: " + array1.length); //0
+    System.out.println("size of array2: " + array2.length); //65
+    System.out.println("size of array3: " + array3.length); //9
+}
+}
+
+//find length of array
