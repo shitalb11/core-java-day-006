3df38a739a1840b5bd3ab0def0ed02d73c76e2f6
Shital Bhosale <shitalb075@gmail.com>
2026-02-17 17:54:46 +0530

    demo5

---
+++ src/main/java/org/example/ArrayBasedProgram/Demo5.java
@@ -0,0 +1,15 @@
+package org.example.ArrayBasedProgram;
+
+public class Demo5 {
+    public static void main(String[] args) {
+         int[] array = {10,20,30,40,50,60,70,80,90};
+
+         for(int e : array){
+             System.out.println(e+",");
+         }
+    }
+}
+
+//an enhanced for loop --> for (int e : array)  "You only need to read values"
+//e --> a temporary variable that holds each element one by one
+//For each value inside array, store it in e and print it.
