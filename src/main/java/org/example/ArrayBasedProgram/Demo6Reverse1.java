f4563ffae53adf9dd49baff2673b878205807301
Shital Bhosale <shitalb075@gmail.com>
2026-02-23 22:14:16 +0530

    new

--- src/main/java/org/example/ArrayBasedProgram/Demo6Reverse1.java
+++ src/main/java/org/example/ArrayBasedProgram/Demo6Reverse1.java
@@ -1,6 +1,6 @@
 package org.example.ArrayBasedProgram;
 
-public class Demo6Reverse {
+public class Demo6Reverse1 {
     public static void main(String[] args) {
         int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90};
 
