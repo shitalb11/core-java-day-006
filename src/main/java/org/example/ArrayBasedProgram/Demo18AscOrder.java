fb116d0ac86d99baa08d3d992cc2c08cc234453b
Shital Bhosale <shitalb075@gmail.com>
2026-03-17 21:50:03 +0530

    demo18

---
+++ src/main/java/org/example/ArrayBasedProgram/Demo18AscOrder.java
@@ -0,0 +1,17 @@
+package org.example.ArrayBasedProgram;
+
+import java.util.Arrays;
+
+public class Demo18AscOrder {
+    public static void main(String[] args) {
+
+        int[] arr = {5,7,6,2,1,9};
+
+        Arrays.sort(arr);
+        System.out.println("Ascending Order:");
+
+        for (int i : arr){
+            System.out.println(i + " ");
+        }
+    }
+}
