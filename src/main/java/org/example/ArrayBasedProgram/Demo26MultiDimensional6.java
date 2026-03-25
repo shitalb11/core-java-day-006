d90238d4f8c4d91b33fb0ea9fffe7d9e947470d2
Shital Bhosale <shitalb075@gmail.com>
2026-03-25 22:17:42 +0530

    demo26

---
+++ src/main/java/org/example/ArrayBasedProgram/Demo26MultiDimensional6.java
@@ -0,0 +1,26 @@
+package org.example.ArrayBasedProgram;
+
+public class Demo26MultiDimensional6 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int[][] transpose = new int[matrix.length][matrix[0].length];//1 4 7
+
+        for (int row=0;row< matrix.length;row++){
+            for (int col=0;col< matrix[row].length;col++){
+                //{1, 2, 3}
+                transpose[row][col]=matrix[col][row];
+            }
+        }
+        System.out.println("Transpose:");
+        for (int row=0;row<transpose.length;row++){
+            for (int col=0;col<transpose[row].length;col++){
+                System.out.print(transpose[row][col]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
